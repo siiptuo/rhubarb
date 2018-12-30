2fdc4c720c86b62ce74099ee95e7f329ce46fd40
Tuomas Siipola <siiptuo@kapsi.fi>
2018-12-31 01:44:58 +0200

    Add initial storage

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use hyper::service::service_fn;
 use hyper::{Body, Client, Method, Request, Response, Server, StatusCode};
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use url::{form_urlencoded, Url};
 
 struct Challenge(String);
@@ -27,16 +29,67 @@ impl ChallengeGenerator for RandomChallengeGenerator {
     }
 }
 
+#[derive(Debug)]
+struct Item {
+    callback: String,
+    topic: String,
+    lease_seconds: u32,
+}
+
+trait Storage {
+    fn get(&self, callback: String, topic: String) -> Option<Item>;
+    fn insert(&mut self, item: Item);
+    fn remove(&mut self, item: Item);
+}
+
+struct HashMapStorage {
+    hash_map: HashMap<(String, String), u32>,
+}
+
+impl HashMapStorage {
+    fn new() -> Self {
+        Self {
+            hash_map: HashMap::new(),
+        }
+    }
+}
+
+impl Storage for HashMapStorage {
+    fn get(&self, callback: String, topic: String) -> Option<Item> {
+        println!("get {{ callback: {}, topic: {} }}", callback, topic);
+        self.hash_map
+            .get(&(callback.clone(), topic.clone()))
+            .map(|lease_seconds| Item {
+                callback,
+                topic,
+                lease_seconds: *lease_seconds,
+            })
+    }
+
+    fn insert(&mut self, item: Item) {
+        println!("insert {:?}", item);
+        self.hash_map
+            .insert((item.callback, item.topic), item.lease_seconds);
+    }
+
+    fn remove(&mut self, item: Item) {
+        println!("remove {:?}", item);
+        self.hash_map.remove(&(item.callback, item.topic));
+    }
+}
+
 fn hello(
     req: Request<Body>,
     challenge_generator: impl ChallengeGenerator + Send + 'static,
+    storage: &Arc<Mutex<impl Storage + Send + 'static>>,
 ) -> Box<Future<Item = Response<Body>, Error = hyper::Error> + Send> {
     if req.method() != Method::POST {
         return Box::new(future::ok(
             Response::builder().body(Body::from("Rhubarb")).unwrap(),
         ));
     }
-    let res = req.into_body().concat2().map(|body| {
+    let storage = storage.clone();
+    let res = req.into_body().concat2().map(move |body| {
         let mut callback = None;
         let mut mode = None;
         let mut topic = None;
@@ -74,7 +127,7 @@ fn hello(
             (Some(callback), Some(mode), Some(topic)) => {
                 // Verification of intent
                 let challenge = challenge_generator.generate();
-                let callback = Url::parse_with_params(
+                let verification = Url::parse_with_params(
                     &callback,
                     &[
                         ("hub.mode", mode.as_ref()),
@@ -84,12 +137,19 @@ fn hello(
                     ],
                 )
                 .unwrap();
+                let callback = callback.to_string();
+                let topic = topic.to_string();
                 let req = Client::new()
-                    .get(callback.into_string().parse().unwrap())
+                    .get(verification.into_string().parse().unwrap())
                     .and_then(move |res| {
                         res.into_body().concat2().map(move |body| {
                             if body.into_bytes() == challenge.0.as_bytes() {
                                 println!("callback ok: challenge accepted");
+                                storage.lock().unwrap().insert(Item {
+                                    callback,
+                                    topic,
+                                    lease_seconds: 123,
+                                });
                             } else {
                                 println!("callback failed: invalid challenge");
                             }
@@ -110,9 +170,15 @@ fn hello(
 
 fn main() {
     let addr = ([127, 0, 0, 1], 3000).into();
+    let storage = Arc::new(Mutex::new(HashMapStorage::new()));
+
+    let service = move || {
+        let storage = Arc::clone(&storage);
+        service_fn(move |req| hello(req, RandomChallengeGenerator::new(), &storage))
+    };
 
     let server = Server::bind(&addr)
-        .serve(|| service_fn(|req| hello(req, RandomChallengeGenerator::new())))
+        .serve(service)
         .map_err(|e| eprintln!("server error: {}", e));
 
     println!("Listening on http://{}", addr);
@@ -127,7 +193,6 @@ mod tests {
     use hyper::rt::{Future, Stream};
     use hyper::service::service_fn_ok;
     use std::cell::Cell;
-    use std::collections::HashMap;
     use std::rc::Rc;
     use std::thread;
     use tokio::runtime;
@@ -150,40 +215,51 @@ mod tests {
 
     #[test]
     fn request_homepage() {
+        let storage = Arc::new(Mutex::new(HashMapStorage::new()));
         let req = Request::builder()
             .method("GET")
             .body(Body::empty())
             .unwrap();
-        hello(req, StaticChallengeGenerator::new("test".to_string()))
-            .and_then(|res| {
-                assert_eq!(res.status(), StatusCode::OK);
-                res.into_body().concat2().map(|body| {
-                    assert_eq!(body.as_ref(), b"Rhubarb");
-                })
+        hello(
+            req,
+            StaticChallengeGenerator::new("test".to_string()),
+            &storage,
+        )
+        .and_then(|res| {
+            assert_eq!(res.status(), StatusCode::OK);
+            res.into_body().concat2().map(|body| {
+                assert_eq!(body.as_ref(), b"Rhubarb");
             })
-            .poll()
-            .unwrap();
+        })
+        .poll()
+        .unwrap();
     }
 
     #[test]
     fn hub_callback_required() {
+        let storage = Arc::new(Mutex::new(HashMapStorage::new()));
         let req = Request::builder()
             .method("POST")
             .body(Body::from(""))
             .unwrap();
-        hello(req, StaticChallengeGenerator::new("test".to_string()))
-            .and_then(|res| {
-                assert_eq!(res.status(), StatusCode::BAD_REQUEST);
-                res.into_body().concat2().map(|body| {
-                    assert_eq!(body.as_ref(), b"hub.callback required");
-                })
+        hello(
+            req,
+            StaticChallengeGenerator::new("test".to_string()),
+            &storage,
+        )
+        .and_then(|res| {
+            assert_eq!(res.status(), StatusCode::BAD_REQUEST);
+            res.into_body().concat2().map(|body| {
+                assert_eq!(body.as_ref(), b"hub.callback required");
             })
-            .poll()
-            .unwrap();
+        })
+        .poll()
+        .unwrap();
     }
 
     #[test]
     fn hub_mode_required() {
+        let storage = Arc::new(Mutex::new(HashMapStorage::new()));
         let req = Request::builder()
             .method("POST")
             .body(Body::from(
@@ -192,19 +268,24 @@ mod tests {
                     .finish(),
             ))
             .unwrap();
-        hello(req, StaticChallengeGenerator::new("test".to_string()))
-            .and_then(|res| {
-                assert_eq!(res.status(), StatusCode::BAD_REQUEST);
-                res.into_body().concat2().map(|body| {
-                    assert_eq!(std::str::from_utf8(&body), Ok("hub.mode required"));
-                })
+        hello(
+            req,
+            StaticChallengeGenerator::new("test".to_string()),
+            &storage,
+        )
+        .and_then(|res| {
+            assert_eq!(res.status(), StatusCode::BAD_REQUEST);
+            res.into_body().concat2().map(|body| {
+                assert_eq!(std::str::from_utf8(&body), Ok("hub.mode required"));
             })
-            .poll()
-            .unwrap();
+        })
+        .poll()
+        .unwrap();
     }
 
     #[test]
     fn hub_mode_invalid() {
+        let storage = Arc::new(Mutex::new(HashMapStorage::new()));
         let req = Request::builder()
             .method("POST")
             .body(Body::from(
@@ -215,22 +296,27 @@ mod tests {
                     .finish(),
             ))
             .unwrap();
-        hello(req, StaticChallengeGenerator::new("test".to_string()))
-            .and_then(|res| {
-                assert_eq!(res.status(), StatusCode::BAD_REQUEST);
-                res.into_body().concat2().map(|body| {
-                    assert_eq!(
-                        std::str::from_utf8(&body),
-                        Ok("hub.mode should be subscribe or unsubscribe")
-                    );
-                })
+        hello(
+            req,
+            StaticChallengeGenerator::new("test".to_string()),
+            &storage,
+        )
+        .and_then(|res| {
+            assert_eq!(res.status(), StatusCode::BAD_REQUEST);
+            res.into_body().concat2().map(|body| {
+                assert_eq!(
+                    std::str::from_utf8(&body),
+                    Ok("hub.mode should be subscribe or unsubscribe")
+                );
             })
-            .poll()
-            .unwrap();
+        })
+        .poll()
+        .unwrap();
     }
 
     #[test]
     fn hub_topic_required() {
+        let storage = Arc::new(Mutex::new(HashMapStorage::new()));
         let req = Request::builder()
             .method("POST")
             .body(Body::from(
@@ -240,15 +326,19 @@ mod tests {
                     .finish(),
             ))
             .unwrap();
-        hello(req, StaticChallengeGenerator::new("test".to_string()))
-            .and_then(|res| {
-                assert_eq!(res.status(), StatusCode::BAD_REQUEST);
-                res.into_body().concat2().map(|body| {
-                    assert_eq!(body.as_ref(), b"hub.topic required");
-                })
+        hello(
+            req,
+            StaticChallengeGenerator::new("test".to_string()),
+            &storage,
+        )
+        .and_then(|res| {
+            assert_eq!(res.status(), StatusCode::BAD_REQUEST);
+            res.into_body().concat2().map(|body| {
+                assert_eq!(body.as_ref(), b"hub.topic required");
             })
-            .poll()
-            .unwrap();
+        })
+        .poll()
+        .unwrap();
     }
 
     #[test]
@@ -290,29 +380,112 @@ mod tests {
             counter.get()
         });
 
+        let storage = Arc::new(Mutex::new(HashMapStorage::new()));
+
+        let callback = format!("http://{}", addr);
+        let topic = "http://topic.local".to_string();
+
         let req = Request::builder()
             .method("POST")
             .body(Body::from(
                 form_urlencoded::Serializer::new(String::new())
-                    .append_pair("hub.callback", &format!("http://{}", addr))
+                    .append_pair("hub.callback", &callback)
                     .append_pair("hub.mode", "subscribe")
-                    .append_pair("hub.topic", "http://topic.local")
+                    .append_pair("hub.topic", &topic)
                     .finish(),
             ))
             .unwrap();
 
         rt::run(
-            hello(req, StaticChallengeGenerator::new("test".to_string()))
-                .map(|res| {
-                    assert_eq!(res.status(), StatusCode::ACCEPTED);
-                })
-                .map_err(|err| panic!(err)),
+            hello(
+                req,
+                StaticChallengeGenerator::new("test".to_string()),
+                &storage,
+            )
+            .map(|res| {
+                assert_eq!(res.status(), StatusCode::ACCEPTED);
+            })
+            .map_err(|err| panic!(err)),
         );
 
         tx.send(()).unwrap();
         let requests = subscriber.join().unwrap();
         assert_eq!(requests, 1);
 
-        // TODO: check subscription
+        assert!(storage.lock().unwrap().get(callback, topic).is_some());
+    }
+
+    #[test]
+    fn subscribe_invalid_response() {
+        let addr = ([127, 0, 0, 1], 3001).into();
+        let (tx, rx) = futures::sync::oneshot::channel::<()>();
+        let subscriber = thread::spawn(move || {
+            let exec = runtime::current_thread::TaskExecutor::current();
+            let counter = Rc::new(Cell::new(0));
+            let counter2 = counter.clone();
+            let server = Server::bind(&addr)
+                .executor(exec)
+                .serve(move || {
+                    let cnt = counter2.clone();
+                    service_fn_ok(move |req| {
+                        let query = req.uri().query();
+                        assert!(query.is_some());
+                        let params = form_urlencoded::parse(query.unwrap().as_bytes())
+                            .into_owned()
+                            .collect::<HashMap<String, String>>();
+                        assert_eq!(params.get("hub.mode"), Some(&"subscribe".to_string()));
+                        assert_eq!(
+                            params.get("hub.topic"),
+                            Some(&"http://topic.local".to_string())
+                        );
+                        assert_eq!(params.get("hub.challenge"), Some(&"test".to_string()));
+                        assert_eq!(params.get("hub.lease_seconds"), Some(&"123".to_string()));
+                        cnt.set(cnt.get() + 1);
+                        Response::new(Body::from("invalid"))
+                    })
+                })
+                .with_graceful_shutdown(rx)
+                .map_err(|err| eprintln!("server error: {}", err));
+            runtime::current_thread::Runtime::new()
+                .expect("rt new")
+                .spawn(server)
+                .run()
+                .expect("rt run");
+            counter.get()
+        });
+
+        let storage = Arc::new(Mutex::new(HashMapStorage::new()));
+
+        let callback = format!("http://{}", addr);
+        let topic = "http://topic.local".to_string();
+
+        let req = Request::builder()
+            .method("POST")
+            .body(Body::from(
+                form_urlencoded::Serializer::new(String::new())
+                    .append_pair("hub.callback", &callback)
+                    .append_pair("hub.mode", "subscribe")
+                    .append_pair("hub.topic", &topic)
+                    .finish(),
+            ))
+            .unwrap();
+
+        rt::run(
+            hello(
+                req,
+                StaticChallengeGenerator::new("test".to_string()),
+                &storage,
+            )
+            .map(|res| {
+                assert_eq!(res.status(), StatusCode::ACCEPTED);
+            })
+            .map_err(|err| panic!(err)),
+        );
+
+        tx.send(()).unwrap();
+        let requests = subscriber.join().unwrap();
+        assert_eq!(requests, 1);
+
+        assert!(storage.lock().unwrap().get(callback, topic).is_none());
     }
 }
